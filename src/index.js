2f765106b6099510b176ad0abd08d227f7185fcf
Arsalan Karimzad <karimzadarsalan@gmail.com>
2020-10-10 17:54:03 +0330

    added redux-persist and session storage to save redux store in session

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from 'react-dom'
+
 import App from "./App";
 import {Provider} from 'react-redux'
-import store from "./redux/store";
+// import store from "./redux/store";
+import {PersistGate} from 'redux-persist/integration/react'
+import {persistor,store} from "./redux/store";
 
 ReactDOM.render(
     <Provider store={store}>
+        <PersistGate persistor={persistor}>
         <App/>
+        </PersistGate>
     </Provider>,
     document.querySelector("#root")
 )
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import {createStore, applyMiddleware} from "redux";
 import {composeWithDevTools} from 'redux-devtools-extension';
+import {persistStore} from "redux-persist";
 
 import logger from 'redux-logger'
 import rootReducer from './root-reducer'
 
 const middlewares = [logger]
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)))
-export default store
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)))
+export const persistor = persistStore(store)
+export default {store,persistor}
